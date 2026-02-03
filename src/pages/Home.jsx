cc2cfaeb16e85ffa2288aec40b8fc94ba1f57877
Julia <juliamariadev@gmail.com>
2026-02-03 11:52:21 -0300

    perf: adiciona tag main para acessibilidade

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 // Components
-import Hero from "../sections/Hero"
-import WhyTherapy from "../sections/WhyTherapy"
-import Testimonials from "../sections/Testimonials"
-import About from "../sections/About"
-import InstagramSection from "../sections/InstagramSection"
-import Faq from "../sections/Faq"
-import Cta from "../sections/Cta"
-import Footer from "../layouts/Footer"
+import Hero from '../sections/Hero';
+import WhyTherapy from '../sections/WhyTherapy';
+import Testimonials from '../sections/Testimonials';
+import About from '../sections/About';
+import InstagramSection from '../sections/InstagramSection';
+import Faq from '../sections/Faq';
+import Cta from '../sections/Cta';
+import Footer from '../layouts/Footer';
 
 function Home() {
   return (
     <>
-      <Hero />
-      <WhyTherapy />
-      <Testimonials />
-      <About />
-      <InstagramSection />
-      <Faq />
-      <Cta />
+      <main id="main-content">
+        <Hero />
+        <WhyTherapy />
+        <Testimonials />
+        <About />
+        <InstagramSection />
+        <Faq />
+        <Cta />
+      </main>
+
       <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
